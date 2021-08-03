2d6b425df2d655c33877a89731304eff6ffddbae
harshitverma2168 <harshit.verma@quovantis.com>
2021-08-04 02:34:01 +0530

    TestCase 2 | should return number on number

--- src/calculator/StringCalculator.java
+++ src/calculator/StringCalculator.java
@@ -1,7 +1,10 @@
 package calculator;
 
 public class StringCalculator {
-    public static int Add(String numbers){
-        return 0;
+    public static int Add(String numbers) {
+        if (numbers.isEmpty())
+            return 0;
+        else
+            return Integer.parseInt(numbers);
     }
 }
--- src/calculator/StringCalculatorTest.java
+++ src/calculator/StringCalculatorTest.java
@@ -1,10 +1,17 @@
 package calculator;
 
 import org.junit.*;
+
 import static org.junit.Assert.*;
+
 public class StringCalculatorTest {
     @Test
-    public void shouldReturnZeroForEmptyString(){
-        assertEquals(0,StringCalculator.Add(""));
+    public void shouldReturnZeroForEmptyString() {
+        assertEquals(0, StringCalculator.Add(""));
+    }
+
+    @Test
+    public void shouldReturnNumberForNumber() {
+        assertEquals(1, StringCalculator.Add("1"));
     }
 }
